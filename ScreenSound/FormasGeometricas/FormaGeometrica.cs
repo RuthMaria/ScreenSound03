695dea49844098d87dea4037cdb7e4fd2257a504
RuthMaria <ruth-15rms@hotmail.com>
2024-11-02 21:46:44 -0300

    adiciona formas geometricas

---
+++ ScreenSound/FormasGeometricas/FormaGeometrica.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Runtime.Intrinsics.X86;
+
+namespace ScreenSound.FormasGeometricas;
+
+/*Uma classe abstrata em é uma classe que não pode ser instanciada diretamente,
+ * mas serve como modelo para outras classes que a herdam.
+*/
+abstract class FormaGeometrica
+{
+    public abstract double CalcularArea();
+    public abstract double CalcularPerimetro();
+}
